22c5b8bf4cb3eae724e93e2bbf0021417c8f3786
Jordan Daubinet <soulmencc+discord@gmail.com>
2025-04-11 09:46:16 +0200

    Replaced Google Social with discord

--- public_html/app/config/config.php
+++ public_html/app/config/config.php
@@ -9,8 +9,9 @@ define('BASE_DOMAIN',      "localhost");       // The primary domain
 define('STATIC_SUBDOMAIN', "static");               // Static subdomain prefix | Has to be a valid subdomain rooted to the same dir of the application's root folder
 define('APP_GAMENAME',     "Bloodbullet");          // Gamename, obviously
 define('APP_DOMAIN',       BASE_DOMAIN);     // Application runs without www.
-define('APP_FB_PAGE',      "");          // Facebook page UNUSED
-define('APP_TW_PAGE',      "CodeKeySolution");          // Twitter page UNUSED
+define('APP_FB_PAGE',      "1754087098848807");          // Facebook page UNUSED
+define('APP_TW_PAGE',      "CodeKeySolution");
+define('APP_DISCORD_INVITE', 'VBkGzG8AS9');         // Twitter page UNUSED
 define('APP_GP_PAGE',      ""); // Google plus page UNUSED
 define('SSL_ENABLED',      false);                   // HTTPS? true :-) / false :-( ?
 define('DEVELOPMENT',      true);                   // Development mode true = on | false = off
--- public_html/app/config/routing.php
+++ public_html/app/config/routing.php
@@ -14,12 +14,12 @@ class Routing
     private $routeRegex = array();
     private $routeLang = "(?:\/(nl|en))?";
     private $routeGet = "(?:\?.*)?";
-    
+
     public $routeMap = array();
     public $ajaxRouteMap = array();
     public $allowedLangs = array("nl", "en");
     public $allowedFileLangs = array("nl" => "nl.php", "en" => "en.php");
-    
+
     public $settings = array(
         'domainBase' => BASE_DOMAIN, // see config.php
         'gamename' => APP_GAMENAME, // see config.php
@@ -27,28 +27,28 @@ class Routing
         'fbPage' => APP_FB_PAGE,// see config.php
         'twPage' => APP_TW_PAGE, // see config.php
         'gpPage' => APP_GP_PAGE, // see config.php
-        
+        'discordInvite' =>APP_DISCORD_INVITE, // see config.php
         //Api settings & other core web settings
         'ssl' => SSL_ENABLED, // see config.php
         'twigCache' => FALSE, // Init, DO NOT CHANGE see config.php 'DEVELOPMENT'
     );
-    
+
     public function __construct()
     {
         $this->settings['twigCache'] = DEVELOPMENT === false ? DOC_ROOT . '/app/cache/TwigCompilation/' : FALSE;
         require_once __DIR__.'/routes/routes.php';
-        
+
         $endRoute = $_SERVER['REQUEST_URI'];
         $this->setRoute($endRoute, $this->routeMap);
         if($this->routeName == "not_found")
             $this->setRoute($endRoute, $this->ajaxRouteMap, true);
     }
-    
+
     public function __destruct()
     {
         $this->routeLang = $this->routeGet = $this->routeMap = $this->ajaxRouteMap = null;
     }
-    
+
     private function setRoute($route, $routeMap, $ajaxRoute = false)
     {
         $this->route = $route;
@@ -59,27 +59,27 @@ class Routing
             $this->routeName = $routeName;
         }
     }
-    
+
     public function getRoute()
     {
         return $this->route;
     }
-    
+
     public function getRouteName()
     {
         return $this->routeName;
     }
-    
+
     public function getController()
     {
         return $this->controller;
     }
-    
+
     public function getReplacedRoute()
     {
         return $this->replaceRouteRegex($this->route);
     }
-    
+
     private function removeRouteRegex($route)
     {
         foreach($this->routeRegex AS $regex)
@@ -89,7 +89,7 @@ class Routing
         }
         return $route;
     }
-    
+
     private function replaceRouteRegex($route)
     {
         $routeLang = substr($route, 1, 2);
@@ -99,24 +99,24 @@ class Routing
         {
             if($regex == $this->routeLang && in_array($routeLang, $this->allowedLangs))
                 $return = preg_replace('/' . $regex . '/', '', $route, 1);
-            
+
             if($regex == $this->routeGet && preg_match("/" . $regex . "/", $lastPar))
                 $return = str_replace($lastPar, preg_replace('/' . $regex . '/', '', $lastPar), $route);
-            
+
             $return = isset($return) ? $return : $route;
         }
         return $return;
     }
-    
+
     public function getRouteByRouteName($routeName)
     {
         $result = isset($this->routeMap[$routeName]) ? $this->routeMap[$routeName]['route'] : null;
         if(isset($result))
             $result = $this->removeRouteRegex($result);
-        
+
         return $result;
     }
-    
+
     public function getRouteNameByRoute($route)
     {
         foreach($this->routeMap AS $key => $value)
@@ -125,12 +125,12 @@ class Routing
                 return $key;
         }
     }
-    
+
     public function getAjaxRouteByRouteName($routeName)
     {
         return isset($this->ajaxRouteMap[$routeName]) ? $this->ajaxRouteMap[$routeName]['route'] : null;
     }
-    
+
     public function getAjaxRouteNameByRoute($route)
     {
         foreach($this->ajaxRouteMap AS $key => $value)
@@ -139,12 +139,12 @@ class Routing
                 return $key;
         }
     }
-    
+
     public function getPrevRouteName()
     {
         return isset($_SESSION['PREV_ROUTE_NAME']) ? $_SESSION['PREV_ROUTE_NAME'] : $this->routeName;
     }
-    
+
     private function setPrevRouteNameByRoute($route)
     {
         if(isset($_SESSION['PREV_ROUTE']) && preg_match('{^'.$route.'$}', $_SESSION['PREV_ROUTE']))
@@ -153,12 +153,12 @@ class Routing
         }
         return TRUE;
     }
-    
+
     public function getPrevRoute()
     {
         return isset($_SESSION['PREV_ROUTE']) ? $_SESSION['PREV_ROUTE'] : "/";
     }
-    
+
     public function setPrevRoute()
     {
         $result = isset($this->routeMap[$this->routeName]) ? $this->routeMap[$this->routeName]['route'] : null;
@@ -169,20 +169,20 @@ class Routing
         }
         return TRUE;
     }
-    
+
     public function headTo($routeName, $addAfterRoute = "")
     {
         $result = isset($this->routeMap[$routeName]) ? $this->routeMap[$routeName]['route'] : null;
         if($result != null)
         {
             $result = $this->removeRouteRegex($result);
-            
+
             header("HTTP/2 301 Moved Permanently");
             header('Location: ' . $result . $addAfterRoute, TRUE, 301);
             exit(0);
         }
     }
-    
+
     public function requestGetParam($param, $range=FALSE)
     {
         $requestURI = explode('/', $this->getRoute());
@@ -200,12 +200,12 @@ class Routing
                 if($what < $range['min'] || $what > $range['max'])
                     $this->headTo('not_found');
             }
-            
+
             return $what;
         }
         return FALSE;
     }
-    
+
     function getLanguageByIp()
     {
         $user = new UserCoreService();
@@ -213,28 +213,28 @@ class Routing
         $language = "English";
         if(isset($host) && (preg_match("/nl$/", $host) || preg_match("/be$/", $host) || preg_match("/arpa$/", $host)))
             $language = "Dutch";
-        
+
         return $language;
     }
-    
+
     private function getFirstVisitLang()
     {
         $language = $this->getLanguageByIp();
         $lang = "en";
-        
+
         if($language == "Dutch")
             $lang = "nl";
-        
+
         setcookie('lang', $lang, time()+9999999, '/', $this->settings['domain'], SSL_ENABLED, true);
         return $lang;
     }
-    
+
     public function setLang($lang)
     {
         if(in_array($lang, $this->allowedLangs))
             setcookie('lang', $lang, time()+9999999, '/', $this->settings['domain'], SSL_ENABLED, true);
     }
-    
+
     public function getLang()
     {
         return isset($_COOKIE['lang']) && in_array($_COOKIE['lang'], $this->allowedLangs) ? $_COOKIE['lang'] : $this->getFirstVisitLang();
@@ -244,11 +244,11 @@ class Routing
     {
         return $this->allowedFileLangs[$lang] ?? $this->allowedFileLangs['en'];
     }
-    
+
     public function adjustLang($lang)
     {
         global $uriLang;
-        
+
         if(in_array($uriLang, $this->allowedLangs) && $this->requestGetParam(1) != "game")
         { // Outgame multilingual SEO purposes
             $lang = $uriLang;
@@ -257,11 +257,11 @@ class Routing
         $lang = $this->adjustUserLang($lang);
         return $lang;
     }
-    
+
     private function adjustUserLang($lang)
     {
         global $userData;
-        
+
         if(!isset($_SESSION['lang']['setAfterLogin']) && is_object($userData))
         {
             if($loggedInLang = $userData->getLang() != $lang && in_array($userData->getLang(), $this->allowedLangs))
@@ -274,18 +274,18 @@ class Routing
         }
         return $lang;
     }
-    
+
     public function getLangRaw()
     {
         global $lang;
         return isset($lang) && $lang == "nl" ? "nl-NL" : "en-EN";
     }
-    
+
     public function createActionMessage($msg)
     {
         $_SESSION['message'] = $msg;
     }
-    
+
     public function setActionMessage()
     {
         $message = "";
@@ -293,35 +293,35 @@ class Routing
         unset($_SESSION['message']);
         return $message;
     }
-    
+
     public function replaceMessagePart($part, $message, $pattern)
     {
         return preg_replace($pattern, $part, $message);
     }
-    
+
     public function replaceMessageParts($message)
     {
         foreach($message AS $part)
         {
             if(isset($replaced))
                 $replaced = preg_replace((string)$part['pattern'], (string)$part['part'], (string)$replaced);
-            
+
             if(!isset($replaced))
                 $replaced = preg_replace((string)$part['pattern'], (string)$part['part'], (string)$part['message']);
         }
         return $replaced;
     }
-    
+
     public static function errorMessage($msg)
     {
         return array('alert' => array('danger' => true, 'message' => $msg));
     }
-    
+
     public static function successMessage($msg)
     {
         return array('alert' => array('success' => true, 'message' => $msg));
     }
-    
+
     public static function warningMessage($msg)
     {
         return array('alert' => array('warning' => true, 'message' => $msg));
